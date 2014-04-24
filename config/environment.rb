ae0f3e5daced4d8d2688a067e714e6767380c6fe
Adam Hellberg <sharparam@sharparam.com>
2014-04-24 02:48:08 +0200

    Update configs

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+SECRET_CONFIG = YAML.load_file("#{Rails.root}/config/secrets.yml")[Rails.env]
+
 # Initialize the Rails application.
 TriDevsSite::Application.initialize!
---
+++ config/initializers/secret_key_base.rb
@@ -0,0 +1,1 @@
+TriDevsSite::Application.config.secret_key_base = SECRET_CONFIG["secret_key_base"]
